4a55865de74a13250faea6e9910b9d5508710464
Robert Kimaiyo <97959136+robert5313@users.noreply.github.com>
2023-08-11 00:04:38 +0300

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,11 @@
+--Write a Script that creates an SQL table named "users"
+--With the following attributes:
+--id = (integer, auto increment, primary key, never null)
+--email = (string of 255 characters, unique, never null)
+--name = (string of 255 characters)
+--country (enumeration of US, CO, and TN, defaulting to US and never null)
+--If the table already exists, the script should not fail.
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
